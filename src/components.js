38bbd0da9e96fbe6e370cef747c7c365f06950b9
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-07-23 00:35:35 -0700

    Place systems based on their distance.

--- src/components.js
+++ src/components.js
@@ -3,5 +3,9 @@ Crafty.c('System', {
     this.requires('2D, Canvas, Color');
     this.color('white');
     this.attr({w: 3, h: 3})
+  },
+
+  at: function(p) {
+    this.attr({x: 10 + p.distance / Game.max_range * Game.width, y: Game.height / 2})
   }
 })
--- src/game.js
+++ src/game.js
@@ -1,25 +1,30 @@
+function ra(hours, minutes, seconds) {
+  return 0;
+}
+
 Game = {
   systems: [
-    { name: "Solar System", planets: 8 },
-    { name: "Alpha Centauri", planets: 1 },
-    { name: "Barnard's Star" },
-    { name: "WISE 1049-5319" },
-    { name: "Wold 359" },
-    { name: "Lalande 21185" },
-    { name: "Sirius" },
-    { name: "Luyten 726-8" },
-    { name: "Ross 154" },
-    { name: "Ross 248" },
-    { name: "WISE 1506+7027" },
-    { name: "Epsilon Eridani", planets: 2 },
-    { name: "Lacaille 9352" },
-    { name: "Ross 128" },
-    { name: "WISE 0350-5658" },
-    { name: "EZ Aquarii" },
+    { name: "Solar System",   p:{ra:ra(0, 0, 0), dec: 0, distance:  0      }, planets: 8 },
+    { name: "Alpha Centauri", p:{ra:ra(0, 0, 0), dec: 0, distance:  4.2421 }, planets: 1 },
+    { name: "Barnard's Star", p:{ra:ra(0, 0, 0), dec: 0, distance:  5.9630 } },
+    { name: "WISE 1049-5319", p:{ra:ra(0, 0, 0), dec: 0, distance:  6.52   } },
+    { name: "Wold 359",       p:{ra:ra(0, 0, 0), dec: 0, distance:  7.7825 } },
+    { name: "Lalande 21185",  p:{ra:ra(0, 0, 0), dec: 0, distance:  8.2905 } },
+    { name: "Sirius",         p:{ra:ra(0, 0, 0), dec: 0, distance:  8.5828 } },
+    { name: "Luyten 726-8",   p:{ra:ra(0, 0, 0), dec: 0, distance:  8.7280 } },
+    { name: "Ross 154",       p:{ra:ra(0, 0, 0), dec: 0, distance:  9.6813 } },
+    { name: "Ross 248",       p:{ra:ra(0, 0, 0), dec: 0, distance: 10.322  } },
+    { name: "WISE 1506+7027", p:{ra:ra(0, 0, 0), dec: 0, distance: 10.521  } },
+    { name: "Epsilon Eridani",p:{ra:ra(0, 0, 0), dec: 0, distance: 10.522  }, planets: 2 },
+    { name: "Lacaille 9352",  p:{ra:ra(0, 0, 0), dec: 0, distance: 10.742  } },
+    { name: "Ross 128",       p:{ra:ra(0, 0, 0), dec: 0, distance: 10.919  } },
+    { name: "WISE 0350-5658", p:{ra:ra(0, 0, 0), dec: 0, distance: 11.208  } },
+    { name: "EZ Aquarii",     p:{ra:ra(0, 0, 0), dec: 0, distance: 11.266  } },
   ],
 
   width: 800,
   height: 600,
+  max_range: 12,
 
   start: function() {
     Crafty.init(Game.width, Game.height);
@@ -28,7 +33,7 @@ Game = {
     var x = 20;
     var y = Game.height/2;
     Game.systems.forEach(function(system) {
-      Crafty.e('System').attr({x: x, y: y});
+      Crafty.e('System').at(system.p);
       x += 20;
     });
   }
