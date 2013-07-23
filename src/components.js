20d758914b5683ffa0e38c7eda63af940e448904
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-07-23 01:06:28 -0700

    Color systems based on spectral class

--- src/components.js
+++ src/components.js
@@ -5,6 +5,21 @@ Crafty.c('System', {
     this.attr({w: 3, h: 3})
   },
 
+  from: function(system) {
+    this.at(system.p)
+    switch(system.cl) {
+      case "A": this.color('#f8f7ff'); break;
+      case "D": this.color('#F0F0F0'); break;
+      case "F": this.color('#fcf4d8'); break;
+      case "G": this.color('#fff2a1'); break;
+      case "K": this.color('#ffe46f'); break;
+      case "L": this.color('#ff6060'); break;
+      case "M": this.color('#ffa040'); break;
+      case "T": this.color('#ff4080'); break;
+      case "Y": this.color('#9366B4'); break;
+    }
+  },
+
   at: function(p) {
     var x0 = Game.width / 2
     var y0 = Game.height / 2
