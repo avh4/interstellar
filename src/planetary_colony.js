ca8fd4f0b64bdad053f48d97b80902f89497e313
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-07-27 13:09:06 -0700

    Fix player's capture rate

--- src/planetary_colony.js
+++ src/planetary_colony.js
@@ -7,6 +7,6 @@ module.exports.prototype.systemRadiatedEnergy = function(systemName, luminosity_
   var capture_W_e26 = luminosity_W_e26 * (this.capture_pernano / 1000000000);
   var energy_J_e41 = capture_W_e26 * Δtime_seconds_e15;
   this.player.harnessedEnergy_J_e41 += energy_J_e41;
-  this.player.currentCapture_W_e26 += luminosity_W_e26;
-  this.player[systemName].currentCapture_W_e26 += luminosity_W_e26;
+  this.player.currentCapture_W_e26 += capture_W_e26;
+  this.player[systemName].currentCapture_W_e26 += capture_W_e26;
 }
